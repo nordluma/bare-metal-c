c4abb504188ef7464a32e4a791065a12a8cb8e3a
nordluma <maxnordlund.95@hotmail.com>
2024-03-10 22:10:39 +0200

    style(hello): add comments

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+/* Hello World */
+
+/*
+ * Hello World -- start of every language
+ *
+ * Usage:
+ *      1. Run the program.
+ *      2. See the world and go touch grass.
+ */
+
 int main() {
-    printf("Hello World!\n");
-    return (0);
+  printf("Hello World!\n");
+  return (0);
 }
